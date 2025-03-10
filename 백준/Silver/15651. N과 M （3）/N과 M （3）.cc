ed67f0580e9e614ef08b2c9d21cc9badc577552e
Namjooseung <106240615+Namjooseung@users.noreply.github.com>
2025-03-10 22:50:31 +0900

    [Silver III] Title: N과 M (3), Time: 368 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N,M;
+int arr[10];
+bool isused[10];
+
+
+void func(int k){
+	if(k==M){
+		for(int i=0;i<M;i++) cout<<arr[i]<<' ';
+		cout<<'\n';
+		return;
+	}
+	for(int i=1;i<=N;i++){
+		//if(isused[i]) continue;
+		arr[k]=i;
+		//isused[i]=true;
+		func(k+1);
+		//isused[i]=false;
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+ 	cin.tie(0);
+	cin>>N>>M;
+	func(0);
+}
