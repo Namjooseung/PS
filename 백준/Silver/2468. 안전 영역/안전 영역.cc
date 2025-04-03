27f6ac7801fc3d4f960de1f5a85d62a5420f9715
Namjooseung <106240615+Namjooseung@users.noreply.github.com>
2025-04-03 20:33:28 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -3,63 +3,59 @@ using namespace std;
 
 int N;
 int board[102][102];
-int visited[102][102];
-int min_height=101;
-int max_height=0;
-
+int visited[102][102]={0,};
 int dx[4]={1,0,-1,0};
 int dy[4]={0,1,0,-1};
 
-int CheckSafeArea(int height){
-    int safe_area_num=0;
+void bfs(int x,int y,int d){
+    queue<pair<int,int>> q;
+    q.push(make_pair(x,y));
+    visited[x][y]=1;
+    while(!q.empty()){
+        auto cur=q.front(); q.pop();
+        for(int i=0;i<4;i++){
+            int nx=cur.first+dx[i];
+            int ny=cur.second+dy[i];
+            if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
+            if(visited[nx][ny]==1 || board[nx][ny]<=d) continue;
+            q.push(make_pair(nx,ny));
+            visited[nx][ny]=1;
+        }
+    }
+}
+
+int SafeArea(int d){
+    memset(visited,0,sizeof(visited));
+    int area_num=0;
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            if(board[i][j]>height && visited[i][j]==0){
-                safe_area_num++;
-                queue <pair<int,int>> Q;
-                Q.push(make_pair(i,j));
-                visited[i][j]=1;
-                while(!Q.empty()){
-                    auto cur=Q.front(); Q.pop();
-                    for(int i=0;i<4;i++){
-                        int nx=cur.first+dx[i];
-                        int ny=cur.second+dy[i];
-                        if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
-                        if(board[nx][ny]<=height || visited[nx][ny]==1) continue;
-                        visited[nx][ny]=1;
-                        Q.push(make_pair(nx,ny));
-                    }
-                }
+            if(board[i][j]>d&&visited[i][j]!=1){
+                bfs(i,j,d);
+                area_num++;
             }
         }
     }
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++) visited[i][j]=0;
-    }
-
-    return safe_area_num;
+    
+    return area_num;
 }
 
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
     cin>>N;
-
     for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            cin>>board[i][j];
-            if(board[i][j]>max_height) max_height=board[i][j];
-            if(board[i][j]<min_height) min_height=board[i][j];
-            visited[i][j]=0;
-        }
+        for(int j=0;j<N;j++) cin>>board[i][j];
     }
 
-    int max_num=0;
-
-    for(int i=min_height-1;i<=max_height;i++){
-        int num=CheckSafeArea(i);
-        if(max_num<num) max_num=num;
+    int max_area=0;
+    for(int i=0;i<102;i++){
+        int now_area=SafeArea(i);
+        if(now_area==0) break;
+        if(max_area<now_area) max_area=now_area;
     }
 
-    cout<<max_num;
+    cout<<max_area;
 
     return 0;
 }
